c99a0c20b59748a73639aa8c77cc092aa5332cb9
w0rng <antonabramov2000@gmail.com>
2021-07-07 12:39:30 +0700

    fix user model

--- odoo_auth/backends.py
+++ odoo_auth/backends.py
@@ -1,9 +1,12 @@
 import xmlrpc.client
 
+from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.models import User
 
 from . import settings
+
+
+User = get_user_model()
 
 
 class OdooBackend(BaseBackend):
