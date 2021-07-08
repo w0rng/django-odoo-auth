28844a80e80f991ab80f73b09a1b8358bae8ddd2
w0rng <antonabramov2000@gmail.com>
2021-07-08 18:22:34 +0700

    add default value for port

--- odoo_auth/settings.py
+++ odoo_auth/settings.py
@@ -2,5 +2,5 @@ from django.conf import settings
 
 
 ODOO_SERVER_URL = getattr(settings, 'ODOO_SERVER_URL', 'http://localhost')
-ODOO_SERVER_PORT = getattr(settings, 'ODOO_SERVER_PORT')
+ODOO_SERVER_PORT = getattr(settings, 'ODOO_SERVER_PORT', None)
 ODOO_SERVER_DBNAME = getattr(settings, 'ODOO_SERVER_DBNAME', 'demo')
